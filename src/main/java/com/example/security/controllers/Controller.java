9296f142c4387ed3b8ffe242effb4901c72e62bd
LisovichOleksandr <lisovich@gmail.com>
2024-11-13 12:32:43 +0200

    commit started app

--- src/main/java/com/example/security/controllers/Controller.java
+++ src/main/java/com/example/security/controllers/Controller.java
@@ -1,4 +1,15 @@
 package com.example.security.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class Controller {
+
+    @GetMapping("/unsecured")
+    public String unsecuredData(){
+        return "Unsecured data";
+    }
 }
